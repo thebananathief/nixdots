7cabe21c67e586aec3b93954a53319ea6687239b
thebananathief <cameron.salomone@gmail.com>
2023-10-18 14:06:22 -0400

    fontpreview

--- packages/fontpreview-ueberzug.nix
+++ packages/fontpreview-ueberzug.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchFromGitHub, autoPatchelfHook, makeWrapper, fzf, imagemagick, ueberzugpp }:
+{ stdenv, lib, fetchFromGitHub, makeWrapper, fzf, imagemagick, ueberzugpp }:
 stdenv.mkDerivation rec {
   pname = "fontpreview-ueberzug";
   version = "f10f40c";
@@ -11,36 +11,38 @@ stdenv.mkDerivation rec {
     sha256 = "14yvbw6k5c2g4lm06zk2m8mkh83h038zw8wq7hgld8ln5y7wn9rm";
   };
 
+  # src = fetchurl {
+  #   url = "https://github.com/xlucn/fontpreview-ueberzug/archive/refs/heads/master.zip";
+  #   hash = "sha256-14yvbw6k5c2g4lm06zk2m8mkh83h038zw8wq7hgld8ln5y7wn9rm";
+  # };
+
   # nativeBuildInputs = [ makeWrapper ];
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
-  buildInputs = [
-    fzf
-    imagemagick
-    ueberzugpp
-  ];
+  nativeBuildInputs = [ makeWrapper ];
+  buildFlags = [ "PREFIX=$(out)" ];
+  # buildInputs = [ fzf imagemagick ueberzugpp ];
   # buildPhase = ''
   # '';
 
 
-  # preInstall = "mkdir -p $out/bin";
-  # installFlags = [ "PREFIX=$(out)" ];
-  # postInstall = ''
-  #   wrapProgram $out/bin/fontpreview-ueberzugpp \
-  #     --prefix PATH : ${lib.makeBinPath [ imagemagick fzf ueberzugpp ]}
-  # '';
-  unpackPhase = "true";
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $out/fontpreview-ueberzug $out/bin
+  preInstall = "mkdir -p $out/bin";
+  installFlags = [ "PREFIX=$(out)" ];
+  postInstall = ''
+    wrapProgram $out/bin/fontpreview-ueberzugpp \
+      --prefix PATH : ${lib.makeBinPath [ imagemagick fzf ueberzugpp ]}
   '';
+  # unpackPhase = "true";
+  # installPhase = ''
+  #   mkdir -p $out/bin
+  #   ls
+  #   cp fontpreview-ueberzug $out/bin
+  # '';
 
   meta = with lib; {
-    description = "fontpreview-ueberzug";
+    description = "Font previewer written in bash";
     homepage = https://github.com/xlucn/fontpreview-ueberzug/;
     license = licenses.mit;
     maintainers = with lib.maintainers; [];
     platforms = [ "x86_64-linux" ];
+    mainProgram = "fontpreview-ueberzug";
   };
 }
