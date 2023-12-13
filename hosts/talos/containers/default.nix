ff230777922d54a6bf105cb13422bc66ca4a0849
thebananathief <cameron.salomone@gmail.com>
2023-12-13 18:03:57 -0500

    talos deployment

--- hosts/talos/containers/default.nix
+++ hosts/talos/containers/default.nix
@@ -9,7 +9,7 @@ let
   # TODO: See if these are reading in the secrets just to store them in the /nix/store
   
   # Get secret file's path, read the contents and store them
-  mysql_password = "asd123asd123";
+  # mysql_password = "asd123asd123";
   # mysql_password = "$__file{${secrets.mysql_password.path}}";
   # mumble_superpassword = "$__file{${secrets.mumble_superpassword.path}}";
   # email_address = "$__file{${secrets.email_address.path}}";
@@ -17,13 +17,13 @@ let
   # cloudflare_email = "$__file{${secrets.cloudflare_email.path}}";
   # cloudflare_apikey = "$__file{${secrets.cloudflare_apikey.path}}";
   # webtrees_password = "$__file{${secrets.webtrees_password.path}}";
-  postgres_password = "123asd123asd";
+  # postgres_password = "123asd123asd";
   # postgres_password = "$__file{${secrets.postgres_password.path}}";
   # mullvad_privKey = "$__file{${secrets.mullvad_privKey.path}}";
 
   # main_domain = builtins.readFile secrets.main_domain.path;
   # main_domain = "$__file{${secrets.main_domain.path}}";
-  main_domain = "talos.host";
+  # main_domain = "talos.host";
 
   main_uid = "1000";
   main_gid = "100";
