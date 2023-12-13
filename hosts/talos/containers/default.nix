0dc643d44046f39a6025d7c29e71ffdeaf2aa102
thebananathief <cameron.salomone@gmail.com>
2023-12-12 19:01:10 -0500

    secrets changes

--- hosts/talos/containers/default.nix
+++ hosts/talos/containers/default.nix
@@ -6,21 +6,23 @@ let
   download_path = "/mnt/disk1/downloads";
 
   inherit (config.sops) secrets;
-  # Get secret file's path, read the contents and store them
   # TODO: See if these are reading in the secrets just to store them in the /nix/store
-  mysql_password = "$__file{${secrets.mysql_password.path}}";
-  mumble_superpassword = "$__file{${secrets.mumble_superpassword.path}}";
-  email_address = "$__file{${secrets.email_address.path}}";
-  main_domain = "$__file{${secrets.main_domain.path}}";
-  cloudflare_email = "$__file{${secrets.cloudflare_email.path}}";
-  cloudflare_apikey = "$__file{${secrets.cloudflare_apikey.path}}";
-  webtrees_password = "$__file{${secrets.webtrees_password.path}}";
-  postgres_password = "$__file{${secrets.postgres_password.path}}";
-  mullvad_privKey = "$__file{${secrets.mullvad_privKey.path}}";
-
   
-  # Store the secret file's path (/run/secrets/<keyName>)
-  # mysql_password = config.sops.secrets.mysql_password.path;
+  # Get secret file's path, read the contents and store them
+  # mysql_password = "$__file{${secrets.mysql_password.path}}";
+  # mumble_superpassword = "$__file{${secrets.mumble_superpassword.path}}";
+  # email_address = "$__file{${secrets.email_address.path}}";
+  # main_domain = "$__file{${secrets.main_domain.path}}";
+  # cloudflare_email = "$__file{${secrets.cloudflare_email.path}}";
+  # cloudflare_apikey = "$__file{${secrets.cloudflare_apikey.path}}";
+  # webtrees_password = "$__file{${secrets.webtrees_password.path}}";
+  # postgres_password = "$__file{${secrets.postgres_password.path}}";
+  # mullvad_privKey = "$__file{${secrets.mullvad_privKey.path}}";
+
+  main_domain = readFile secrets.main_domain.path;
+  # main_domain = readFile /run/secrets/main_domain;
+  # main_domain = "$__file{${secrets.main_domain.path}}";
+  # main_domain = import /run/secrets/main_domain;
 
   # Read the file at /run/secrets/<keyName> (but through import) and store it
   # Could you also use import declaratively like above?
@@ -80,54 +82,54 @@ in {
           "--health-timeout=10s"
         ];
       };
-      mumble = {
-        image = "mumblevoip/mumble-server:latest"; # https://github.com/Theofilos-Chamalis/mumble-web
-        volumes = [
-          "${ appdata_path }/mumble:/data"
-        ];
-        ports = [ "64738:64738" ];
-        environment = {
-          MUMBLE_CONFIG_WELCOMETEXT = "Welcome to the Shire! Have a grand time and don't disturb the hobbits!";
-          MUMBLE_SUPERUSER_PASSWORD = "${ mumble_superpassword }";
-        };
-        # labels = {
-        #   # https://github.com/PHLAK/docker-mumble/issues/15
-        #   "traefik.enable" = true;
-        # };
-      };
-      webtrees = {
-        image = "dtjs48jkt/webtrees:latest"; # https://hub.docker.com/r/dtjs48jkt/webtrees/
-        volumes = [
-          "/etc/localtime:/etc/localtime:ro"
-          "${ appdata_path }/webtrees/data:/var/www/html/data"
-          "${ appdata_path }/webtrees/modules:/var/www/html/modules_v4"
-        ];
-        environment = {
-          DB_USER = "root";
-          DB_PASSWORD = "${ mysql_password }";
-          DB_HOST = "mysql";
-          DB_PORT = "3306";
-          DB_NAME = "general"; # TODO: migrate "general" to "webtrees" (db name)
-          WT_ADMIN = "thebananathief";
-          WT_ADMINMAIL = "${ email_address }";
-          WT_ADMINPW = "${ webtrees_password }";
-          GROUP_ID = "${ main_gid }";
-          PORT = "8079";
-          DISABLE_SSL = "TRUE";
-          PRETTYURLS = "TRUE";
-          BASE_URL = "https://tree.${ main_domain }";
-        };
-        dependsOn = [ "mysql" ];
-        labels = {
-          "traefik.enable" = "true";
-          "traefik.http.routers.webtrees.rule" = "Host(`tree.${ main_domain }`)";
-          "traefik.http.routers.webtrees.entrypoints" = "websecure";
-          "traefik.http.services.webtrees.loadbalancer.server.port" = "8079";
-        };
-        # extraOptions = [
-        #   "--network=public_access,database_only";
-        # ];
-      };
+      # mumble = {
+      #   image = "mumblevoip/mumble-server:latest"; # https://github.com/Theofilos-Chamalis/mumble-web
+      #   volumes = [
+      #     "${ appdata_path }/mumble:/data"
+      #   ];
+      #   ports = [ "64738:64738" ];
+      #   environment = {
+      #     MUMBLE_CONFIG_WELCOMETEXT = "Welcome to the Shire! Have a grand time and don't disturb the hobbits!";
+      #     MUMBLE_SUPERUSER_PASSWORD = "${ mumble_superpassword }";
+      #   };
+      #   # labels = {
+      #   #   # https://github.com/PHLAK/docker-mumble/issues/15
+      #   #   "traefik.enable" = true;
+      #   # };
+      # };
+      # webtrees = {
+      #   image = "dtjs48jkt/webtrees:latest"; # https://hub.docker.com/r/dtjs48jkt/webtrees/
+      #   volumes = [
+      #     "/etc/localtime:/etc/localtime:ro"
+      #     "${ appdata_path }/webtrees/data:/var/www/html/data"
+      #     "${ appdata_path }/webtrees/modules:/var/www/html/modules_v4"
+      #   ];
+      #   environment = {
+      #     DB_USER = "root";
+      #     DB_PASSWORD = "${ mysql_password }";
+      #     DB_HOST = "mysql";
+      #     DB_PORT = "3306";
+      #     DB_NAME = "general"; # TODO: migrate "general" to "webtrees" (db name)
+      #     WT_ADMIN = "thebananathief";
+      #     WT_ADMINMAIL = "${ email_address }";
+      #     WT_ADMINPW = "${ webtrees_password }";
+      #     GROUP_ID = "${ main_gid }";
+      #     PORT = "8079";
+      #     DISABLE_SSL = "TRUE";
+      #     PRETTYURLS = "TRUE";
+      #     BASE_URL = "https://tree.${ main_domain }";
+      #   };
+      #   dependsOn = [ "mysql" ];
+      #   labels = {
+      #     "traefik.enable" = "true";
+      #     "traefik.http.routers.webtrees.rule" = "Host(`tree.${ main_domain }`)";
+      #     "traefik.http.routers.webtrees.entrypoints" = "websecure";
+      #     "traefik.http.services.webtrees.loadbalancer.server.port" = "8079";
+      #   };
+      #   # extraOptions = [
+      #   #   "--network=public_access,database_only";
+      #   # ];
+      # };
       filebrowser = {
         image = "filebrowser/filebrowser:latest";
         volumes = [
@@ -192,59 +194,59 @@ in {
       #     "traefik.http.services.hedgedoc.loadbalancer.server.port" = "3000";
       #   };
       # };
-      rss = {
-        image = "wangqiru/ttrss:latest";
-        volumes = [
-          "${ appdata_path }/ttrss/feed-icons:/var/www/feed-icons/"
-        ];
-        environment = {
-          SELF_URL_PATH = "https://rss.${ main_domain }/";
-          PUID = "${ main_uid }";
-          PGID = "${ main_gid }";
-          DB_HOST = "postgres";
-          DB_PORT = "5432";
-          DB_USER = "postgres";
-          DB_PASS = "${ postgres_password }";
-        };
-        extraOptions = [
-          # "--network=public_access,database_only"
-          "--tty"
-          "--interactive"
-        ];
-        dependsOn = [ "postgres" ];
-        labels = {
-          "traefik.enable" = "true";
-          "traefik.http.routers.rss.rule" = "Host(`rss.${ main_domain }`)";
-          "traefik.http.routers.rss.entrypoints" = "websecure";
-        };
-      };
-      traefik = {
-        image = "traefik:latest";
-        volumes = [
-          "/var/run/docker.sock:/var/run/docker.sock:ro" # TODO: migrate to podman socket
-          "${ appdata_path }/traefik:/etc/traefik" # TODO: Need to get our traefik.yml file out here BEFORE the container starts
-        ];
-        ports = [
-          "80:80"
-          "443:443"
-          "8080:8080"
-        ];
-        environment = {
-          CLOUDFLARE_EMAIL = "${ cloudflare_email }";
-          CLOUDFLARE_API_KEY = "${ cloudflare_apikey }";
-        };
-        # extraOptions = [ "--network=public_access" ];
+      # rss = {
+      #   image = "wangqiru/ttrss:latest";
+      #   volumes = [
+      #     "${ appdata_path }/ttrss/feed-icons:/var/www/feed-icons/"
+      #   ];
+      #   environment = {
+      #     SELF_URL_PATH = "https://rss.${ main_domain }/";
+      #     PUID = "${ main_uid }";
+      #     PGID = "${ main_gid }";
+      #     DB_HOST = "postgres";
+      #     DB_PORT = "5432";
+      #     DB_USER = "postgres";
+      #     DB_PASS = "${ postgres_password }";
+      #   };
+      #   extraOptions = [
+      #     # "--network=public_access,database_only"
+      #     "--tty"
+      #     "--interactive"
+      #   ];
+      #   dependsOn = [ "postgres" ];
+      #   labels = {
+      #     "traefik.enable" = "true";
+      #     "traefik.http.routers.rss.rule" = "Host(`rss.${ main_domain }`)";
+      #     "traefik.http.routers.rss.entrypoints" = "websecure";
+      #   };
+      # };
+      # traefik = {
+      #   image = "traefik:latest";
+      #   volumes = [
+      #     "/var/run/docker.sock:/var/run/docker.sock:ro" # TODO: migrate to podman socket
+      #     "${ appdata_path }/traefik:/etc/traefik" # TODO: Need to get our traefik.yml file out here BEFORE the container starts
+      #   ];
+      #   ports = [
+      #     "80:80"
+      #     "443:443"
+      #     "8080:8080"
+      #   ];
+      #   environment = {
+      #     CLOUDFLARE_EMAIL = "${ cloudflare_email }";
+      #     CLOUDFLARE_API_KEY = "${ cloudflare_apikey }";
+      #   };
+      #   # extraOptions = [ "--network=public_access" ];
 
-        # TODO: Prefer config only with labels - Take from appdata backup's traefik.yml
+      #   # TODO: Prefer config only with labels - Take from appdata backup's traefik.yml
 
-        # -- OLD DCP FILE --
-        # command:
-        # - "--entrypoints.mumble_tcp.address=:64738"
-        # - "--entrypoints.mumble_udp.address=:64738/udp"
-        # - "--certificatesresolvers.cloudflare.acme.caserver=https://acme-v02.api.letsencrypt.org/directory"
-        # extra_hosts: # https://doc.traefik.io/traefik/providers/docker/#host-networking
-        #   - host.docker.internal:172.18.0.1
-      };
+      #   # -- OLD DCP FILE --
+      #   # command:
+      #   # - "--entrypoints.mumble_tcp.address=:64738"
+      #   # - "--entrypoints.mumble_udp.address=:64738/udp"
+      #   # - "--certificatesresolvers.cloudflare.acme.caserver=https://acme-v02.api.letsencrypt.org/directory"
+      #   # extra_hosts: # https://doc.traefik.io/traefik/providers/docker/#host-networking
+      #   #   - host.docker.internal:172.18.0.1
+      # };
       # adguard = {
       #   image = "adguard/adguardhome:latest"; # https://hub.docker.com/r/adguard/adguardhome
       #   volumes = [
