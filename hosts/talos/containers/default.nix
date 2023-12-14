52a2325ff514a4dc87c431d06fc5951d8846131a
thebananathief <cameron.salomone@gmail.com>
2023-12-14 03:28:30 -0500

    talos deployment

--- hosts/talos/containers/default.nix
+++ hosts/talos/containers/default.nix
@@ -1,36 +1,15 @@
 { config, builtins, ... }:
-let 
-  inherit (config.sops) secrets;
-  # TODO: See if these are reading in the secrets just to store them in the /nix/store
-  
-  # Get secret file's path, read the contents and store them
-  # mysql_password = "asd123asd123";
-  # mysql_password = "$__file{${secrets.mysql_password.path}}";
-  # mumble_superpassword = "$__file{${secrets.mumble_superpassword.path}}";
-  # email_address = "$__file{${secrets.email_address.path}}";
-  # main_domain = "$__file{${secrets.main_domain.path}}";
-  # cloudflare_email = "$__file{${secrets.cloudflare_email.path}}";
-  # cloudflare_apikey = "$__file{${secrets.cloudflare_apikey.path}}";
-  # webtrees_password = "$__file{${secrets.webtrees_password.path}}";
-  # postgres_password = "123asd123asd";
-  # postgres_password = "$__file{${secrets.postgres_password.path}}";
-  # mullvad_privKey = "$__file{${secrets.mullvad_privKey.path}}";
-
-  # main_domain = builtins.readFile secrets.main_domain.path;
-  # main_domain = "$__file{${secrets.main_domain.path}}";
-in {
+{
   imports = [
     ./misc.nix
     ./database.nix
     ./gameserver.nix
-    # ./mediaserver.nix
+    ./mediaserver.nix
     ./monitoring.nix
   ];
 
   virtualisation = {
-    # oci-containers.backend = "podman";
     oci-containers.backend = "docker";
-    # oci-containers.containers = allContainers;
     docker = {
       enable = true;
       autoPrune.enable = true;
@@ -38,6 +17,8 @@ in {
         "--all"
       ];
     };
+    # TODO: Transition to podman - was breaking last i tried
+    # oci-containers.backend = "podman";
     # podman = {
     #   enable = true;
     #   dockerCompat = true;
