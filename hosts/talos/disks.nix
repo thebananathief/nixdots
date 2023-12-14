3642768bea6b044e9868e156a405bd139ddaf694
thebananathief <cameron.salomone@gmail.com>
2023-12-13 19:22:47 -0500

    talos deployment

--- hosts/talos/disks.nix
+++ hosts/talos/disks.nix
@@ -10,6 +10,47 @@
     xfsprogs
     smartmontools
   ];
+  
+  # Storage/Parity drives
+  fileSystems."/mnt/disk1" = {
+    device = "/dev/disk/by-uuid/16165a4b-d650-4a84-a55d-9db8f83d0271";
+    fsType = "ext4";
+  };
+  fileSystems."/mnt/disk2" = {
+    device = "/dev/disk/by-uuid/cec3736e-cfa9-40e4-8143-02b338cd75e9";
+    fsType = "xfs";
+  };
+  fileSystems."/mnt/disk3" = {
+    device = "/dev/disk/by-uuid/bb0b2728-3662-4fef-b862-5f1be6d54172";
+    fsType = "ext4";
+  };
+  # fileSystems."/mnt/disk4" = {
+  #   device = "/dev/disk/by-uuid/db4a98ac-3a13-4a49-a40e-10a06f2db023";
+  #   fsType = "";
+  # };
+  fileSystems."/mnt/parity1" = {
+    device = "/dev/disk/by-uuid/a0f9bda4-acf6-4c9e-af77-8d11c7a10741";
+    fsType = "xfs";
+  };
+
+  # MergerFS pool
+  # Make sure the device defines the mountpoints you want to merge
+  # (anything starting with "disk" in /mnt/)
+  fileSystems."/mnt/storage" = {
+    device = "/mnt/disk*:/mnt/tank/fuse";
+    fsType = "fuse.mergerfs";
+    options = [
+      "defaults"
+      "nonempty"
+      "allow_other"
+      "category.create=epmfs"
+      "use_ino"
+      "moveonenospc=true"
+      "dropcacheonclose=true"
+      "minfreespace=50G"
+      "fsname=mergerfs"
+    ];
+  };
 
   snapraid = {
     enable = true;
@@ -53,25 +94,6 @@
   systemd.services.snapraid-sync.serviceConfig.postStart = ''
     curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/$__file{${config.sops.secrets.healthcheck_uptime_uuid.path}}
   '';
-
-  # Make sure the device defines the mountpoints you want to merge
-  # (anything starting with "disk" in /mnt/)
-  # fileSystems."/mnt/storage" = {
-  #   device = "/mnt/disk*:/mnt/tank/fuse";
-  #   fsType = "fuse.mergerfs";
-  #   options = [
-  #     "defaults"
-  #     "nonempty"
-  #     "allow_other"
-  #     "category.create=epmfs"
-  #     "use_ino"
-  #     "moveonenospc=true"
-  #     "dropcacheonclose=true"
-  #     "minfreespace=50G"
-  #     "fsname=mergerfs"
-  #   ];
-  # };
-
   
   # TODO: Need to have disk SMART alerts sent to me over email
   # Also reminders to buy drives
