0e7c4761f5308db004efecfe33dc6b40d661c837
thebananathief <cameron.salomone@gmail.com>
2023-12-17 04:16:13 -0500

    2023-12-17 04:16:13 EST

--- hosts/talos/mediaserver.nix
+++ hosts/talos/mediaserver.nix
@@ -3,18 +3,26 @@
   inherit (config.sops) secrets;
   main_domain = secrets.main_domain.path;
 in {
+  users.users.mediaserver = {
+    # createHome = false;
+    # uid = 1001;
+    # group = "users";
+    isSystemUser = true;
+    description = "Mediaserver Service account";
+    password = "testpassword";
+  };
   services = {
     plex = {
       enable = true;
       openFirewall = true;
       user = "mediaserver";
-      group = "users";
+      group = "100";
     };
     jellyfin = {
       enable = true;
       openFirewall = true;
       user = "mediaserver";
-      group = "users";
+      group = "100";
     };
     jellyseerr = {
       enable = true;
