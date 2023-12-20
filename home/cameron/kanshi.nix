724877d81b44a22db413aac15d61523c37051383
thebananathief <cameron.salomone@gmail.com>
2023-12-20 15:01:09 -0500

    2023-12-20 15:01:09 EST

--- home/cameron/kanshi.nix
+++ home/cameron/kanshi.nix
@@ -9,7 +9,7 @@
             criteria = "eDP-1";
             mode = "2256x1504@59.99900";
             position = "0,0";
-            scale = 1;
+            scale = 1.0;
             adaptiveSync = false;
             status = "enable";
           }
@@ -21,7 +21,7 @@
             criteria = "eDP-1";
             mode = "2256x1504@59.99900Hz";
             position = "0,0";
-            scale = 1;
+            scale = 1.0;
             adaptiveSync = false;
             status = "enable";
           }
@@ -29,7 +29,7 @@
             criteria = "DP-5";
             mode = "1920x1080@60.00000Hz";
             position = "1128,0";
-            scale = 1;
+            scale = 1.0;
             adaptiveSync = false;
             status = "enable";
           }
@@ -37,7 +37,7 @@
             criteria = "DP-6";
             mode = "1920x1080@60.00000Hz";
             position = "3048,0";
-            scale = 1;
+            scale = 1.0;
             adaptiveSync = false;
             status = "enable";
           }
@@ -45,7 +45,7 @@
             criteria = "DP-7";
             mode = "1920x1080@60.00000Hz";
             position = "1128,0";
-            scale = 1;
+            scale = 1.0;
             adaptiveSync = false;
             status = "enable";
           }
@@ -53,7 +53,7 @@
             criteria = "DP-8";
             mode = "1920x1080@60.00000Hz";
             position = "3048,0";
-            scale = 1;
+            scale = 1.0;
             adaptiveSync = false;
             status = "enable";
           }
