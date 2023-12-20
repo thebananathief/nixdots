6372f4a63e1bd6ff3940381e82cb6c62053ea4d8
thebananathief <cameron.salomone@gmail.com>
2023-12-20 15:16:21 -0500

    2023-12-20 15:16:21 EST

--- home/cameron/kanshi.nix
+++ home/cameron/kanshi.nix
@@ -6,7 +6,7 @@
       undocked = {
         outputs = [
           {
-            criteria = "eDP-1";
+            criteria = "BOE 0x095F";
             mode = "2256x1504@59.99900";
             position = "0,0";
             scale = 1.0;
@@ -18,7 +18,7 @@
       docked = {
         outputs = [
           {
-            criteria = "eDP-1";
+            criteria = "BOE 0x095F";
             mode = "2256x1504@59.99900Hz";
             position = "0,0";
             scale = 1.0;
@@ -26,7 +26,7 @@
             status = "enable";
           }
           {
-            criteria = "DP-5";
+            criteria = "HP Inc. HP V24 1CR0440LFS";
             mode = "1920x1080@60.00000Hz";
             position = "1128,0";
             scale = 1.0;
@@ -34,23 +34,7 @@
             status = "enable";
           }
           {
-            criteria = "DP-6";
-            mode = "1920x1080@60.00000Hz";
-            position = "3048,0";
-            scale = 1.0;
-            adaptiveSync = false;
-            status = "enable";
-          }
-          {
-            criteria = "DP-7";
-            mode = "1920x1080@60.00000Hz";
-            position = "1128,0";
-            scale = 1.0;
-            adaptiveSync = false;
-            status = "enable";
-          }
-          {
-            criteria = "DP-8";
+            criteria = "HP Inc. HP V214a CNC7160VL4";
             mode = "1920x1080@60.00000Hz";
             position = "3048,0";
             scale = 1.0;
