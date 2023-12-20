86135d097bcc2e2deace3aaf462632e8fb5842b5
thebananathief <cameron.salomone@gmail.com>
2023-12-20 15:52:49 -0500

    2023-12-20 15:52:49 EST

--- home/cameron/default.nix
+++ home/cameron/default.nix
@@ -38,7 +38,8 @@
     CLUTTER_BACKEND = "wayland";
     WLR_RENDERER = "vulkan";
     GTK_USE_PORTAL = "1";
-    GDK_BACKEND = "wayland,x11";
+    # GDK_BACKEND = "wayland,x11";
+    GDK_BACKEND = "wayland";
     # GDK_SCALE = "1";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
