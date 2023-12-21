794f13c5f072ae58abcb20906e0cfe851fef2407
thebananathief <cameron.salomone@gmail.com>
2023-12-21 15:23:00 -0500

    2023-12-21 15:23:00 EST

--- home/cameron/spicetify.nix
+++ home/cameron/spicetify.nix
@@ -4,9 +4,9 @@ let
 in
 {
   # allow spotify to be installed if you don't have unfree enabled already
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "spotify"
-  ];
+  # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+  #   "spotify"
+  # ];
 
   # import the flake's module for your system
   imports = [ spicetify-nix.homeManagerModule ];
@@ -23,7 +23,7 @@ in
         fullAppDisplay
         shuffle # shuffle+ (special characters are sanitized out of ext names)
         hidePodcasts
-        
+
         # playlistIcons
         # lastfm
         # genre
