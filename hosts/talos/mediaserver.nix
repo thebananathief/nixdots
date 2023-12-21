e14539ba6753360ef5cb718f5799694aee5913e0
thebananathief <cameron.salomone@gmail.com>
2023-12-21 10:37:46 -0500

    2023-12-21 10:37:46 EST

--- hosts/talos/mediaserver.nix
+++ hosts/talos/mediaserver.nix
@@ -18,31 +18,41 @@ in {
       openFirewall = true;
       user = "mediaserver";
       group = "mediaserver";
+      # TODO: Submit PR so that this module has the other's features
     };
     jellyfin = {
       enable = true;
       openFirewall = true;
       user = "mediaserver";
       group = "mediaserver";
+      # TODO: Submit PR so that this module has the other's features
     };
     jellyseerr = {
       enable = true;
       openFirewall = true;
       port = 8005;
+      # TODO: Submit PR so that this module has the other's features
     };
     sonarr = {
       enable = true;
-      openFirewall = false;
+      openFirewall = true;
       user = "mediaserver";
       group = "mediaserver";
       # dataDir = "/var/lib/sonarr/.config/NzbDrone";
+      # TODO: Submit PR so that this module has the other's features
     };
     radarr = {
       enable = true;
-      openFirewall = false;
+      openFirewall = true;
       user = "mediaserver";
       group = "mediaserver";
       # dataDir = "/var/lib/radarr/.config/Radarr";
+      # TODO: Submit PR so that this module has the other's features
+    };
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
+      # TODO: Submit PR so that this module has the other's features
     };
   };
 }
