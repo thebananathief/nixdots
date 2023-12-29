95f65b05bb09fbead122195cfdc2d9766deb790a
thebananathief <cameron.salomone@gmail.com>
2023-12-29 18:59:16 -0500

    2023-12-29 18:59:16 EST

--- hosts/talos/gameserver.nix
+++ hosts/talos/gameserver.nix
@@ -11,7 +11,7 @@
         motd = "The UPS Store";
         server-port = 25565;
         difficulty = 3;
-        gamemode = 1;
+        gamemode = 0;
         max-players = 24;
         enable-rcon = true;
         "rcon.port" = 25575;
