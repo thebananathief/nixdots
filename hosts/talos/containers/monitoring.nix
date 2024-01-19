5047aec1b023e91eed77ab4c6c05d36d9473d332
thebananathief <cameron.salomone@gmail.com>
2024-01-19 02:19:40 -0500

    2024-01-19 02:19:40 EST

--- hosts/talos/containers/monitoring.nix
+++ hosts/talos/containers/monitoring.nix
@@ -132,9 +132,9 @@ in {
   # Make the service use the docker group ACL, for the socket access
   systemd.services.uptime-kuma.serviceConfig.Group = "docker";
     
-  networking.firewall.allowedTCPPorts = [
-    8017
-  ];
+  # networking.firewall.allowedTCPPorts = [
+  #   8017
+  # ];
   
   # Log management services
   # services.filebeat = {
