37b83b8435a3b321f3646d08cfc9ad18dc92daab
thebananathief <cameron.salomone@gmail.com>
2024-01-30 12:09:47 -0500

    2024-01-30 12:09:47 EST

--- hosts/talos/gitea.nix
+++ hosts/talos/gitea.nix
@@ -1,17 +1,17 @@
 { ... }:
 {
   services.gitea = {
-     enable = true;
-     settings = {
+    enable = true;
+    user = "gitea";
+    group = "gitea";
+    lfs.enable = true;
+    settings = {
       server = {
         DOMAIN = "localhost";
         PROTOCOL = "http";
         HTTP_ADDR = "0.0.0.0"; # listen on details
-        HTTP_PORT = "3000";
+        HTTP_PORT = 3000;
       };
-     };
-    user = "gitea";
-    group = "gitea";
-    lfs.enable = true;
+    };
   };
 }
