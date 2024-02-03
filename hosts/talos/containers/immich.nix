7934dcfd8e32a84ac857f227f8d453a1d335c616
thebananathief <cameron.salomone@gmail.com>
2024-02-02 21:35:24 -0500

    2024-02-02 21:35:24 EST

--- hosts/talos/containers/immich.nix
+++ hosts/talos/containers/immich.nix
@@ -12,6 +12,9 @@ let
     REDIS_HOSTNAME = "immich-redis";
   };
 in {
+  systemd.services.docker-immich-server.postStart = ''
+    docker network connect bridge immich-server
+  '';
   virtualisation.oci-containers.containers = {
     immich-server = { 
       image = "ghcr.io/immich-app/immich-server:${immich_env.IMMICH_VERSION}";
