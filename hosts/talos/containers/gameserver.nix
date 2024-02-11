590ff60786c7e31841d67ca1cee39398537dc70a
thebananathief <cameron.salomone@gmail.com>
2024-02-11 11:28:07 -0500

    2024-02-11 11:28:07 EST

--- hosts/talos/containers/gameserver.nix
+++ hosts/talos/containers/gameserver.nix
@@ -40,7 +40,7 @@ in {
         PRODUCTION = "0";
         HOSTNAME = "Absolute Roleplay - LAWLESS | FEW DLs | COOL WEPS | NO STAFF";
         MAXPLAYERS = "24";
-        GAMEMODE = "darkrp";
+        GAMEMODE = "sandbox";
         MAP = "rp_downtown_tits_v2";
         PORT = "27015";
         GSLT = "***REMOVED***";
