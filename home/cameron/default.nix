6124d9d85864e1d67851953a45036805f6373cab
TheBananaThief <cameron.salomone@gmail.com>
2024-03-22 21:38:05 -0400

    2024-03-22 21:38:05 EDT

--- home/cameron/default.nix
+++ home/cameron/default.nix
@@ -1,6 +1,5 @@
-{ pkgs, lib, inputs, globalFonts, nix-colors, ... }: rec {
+{ pkgs, lib, inputs, globalFonts, ... }: rec {
   imports = [
-    nix-colors.homeManagerModules.default
     ./xdg.nix
     ./git.nix
     ./alacritty.nix
@@ -16,9 +15,8 @@
     ./hyprland.nix
     ./dunst.nix
     # ./vscode.nix
+    ./theme.nix
   ];
-
-  colorScheme = nix-colors.colorSchemes.brogrammer;
 
   home.username = "cameron";
   # home.homeDirectory = "/home/cameron";
@@ -46,19 +44,13 @@
     # GTK_USE_PORTAL = "1";
     GDK_BACKEND = "wayland";
     # GDK_BACKEND = "wayland,x11";
-    # GDK_SCALE = "1";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    # QT_FONT_DPI = "1";
     
     # EXPERIMENTAL: breaks some electron apps
     # Also makes a lot of electron apps use wayland
     NIXOS_OZONE_WL = "1";
     # ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-    
 
     # FROM HYPRLAND.nix
-  
     # QT uses these
     # "XCURSOR_SIZE,24"
     # "XCURSOR_THEME,\"Catppuccin-Mocha-Mauve\""
@@ -86,64 +78,13 @@
   #   target = "./github/dotfiles";
   # };
 
-  fonts.fontconfig.enable = true;
-  
-  home.pointerCursor = {
-    name = "Bibata-Modern-Ice";
-    package = pkgs.bibata-cursors;
-    # name = "Catppuccin-Mocha-Mauve";
-    # package = pkgs.catppuccin-cursors.mochaMauve;
-    size = 24;
-    gtk.enable = true;
-    # x11.enable = true;
-  };
-
-  qt = {
-    enable = true;
-    platformTheme = "qtct";
-    style.name = "kvantum";
-  };
 
   dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      document-font-name = "${globalFonts.sansSerif} 10";
-      monospace-font-name = "${globalFonts.monospace} 10";
-      # font-antialiasing = "rgba";
-      # font-hinting = "full";
-    };
     "org/cinnamon/desktop/applications/terminal".exec = "alacritty";
   };
 
   gtk = {
     enable = true;
-    font = {
-      name = "${globalFonts.sansSerif}";
-      # package = pkgs.lexend;
-      size = 10;
-    };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-    theme = {
-      name = "Catppuccin-Mocha-Compact-Mauve-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        variant = "mocha";
-        accents = ["mauve"];
-        size = "compact";
-      };
-    };
-
-    gtk3.extraCss = ''
-      .nautilus-list-view listview row { margin: 0px; padding: 0; }
-      .nautilus-list-view #NautilusViewCell { padding: 0px; }
-    '';
-    gtk4.extraCss = ''
-      .nautilus-list-view listview row { margin: 0px; padding: 0; }
-      .nautilus-list-view #NautilusViewCell { padding: 0px; }
-    '';
-
     # These are referenced by Thunar for the navigation tree
     gtk3.bookmarks = [
       "file:///home/cameron/code"
@@ -188,16 +129,8 @@
 
     bat = {
       enable = true;
-      config = {
-        theme = "Catppuccin-mocha";
-      };
     };
   };
-
-#   xdg.configFile."bat/themes/Catppuccin-mocha.tmTheme".source = pkgs.fetchurl {
-#     url = "https://raw.githubusercontent.com/catppuccin/bat/main/themes/Catppuccin%20Mocha.tmTheme";
-#     sha256 = "a8c40d2466489a68ebab3cbb222124639221bcf00c669ab45bab242cbb2783fc";
-#   };
 
   # Auto-mounting removeable drives
   services.udiskie.enable = true;
---
+++ home/cameron/theme.nix
@@ -0,0 +1,80 @@
+{ pkgs, lib, inputs, globalFonts, nix-colors, ... }: rec {
+  inputs = [
+    nix-colors.homeManagerModules.default
+  ];
+
+  # https://www.youtube.com/watch?v=jO2o0IN0LPE for help
+  colorScheme = nix-colors.colorSchemes.brogrammer;
+
+  fonts.fontconfig.enable = true;
+  
+  home.pointerCursor = {
+    name = "Bibata-Modern-Ice";
+    package = pkgs.bibata-cursors;
+    # name = "Catppuccin-Mocha-Mauve";
+    # package = pkgs.catppuccin-cursors.mochaMauve;
+    size = 24;
+    gtk.enable = true;
+    # x11.enable = true;
+  };
+
+  qt = {
+    enable = true;
+    platformTheme = "qtct";
+    style.name = "kvantum";
+  };
+
+  gtk = {
+    enable = true;
+    font = {
+      name = "${globalFonts.sansSerif}";
+      # package = pkgs.lexend;
+      size = 10;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    theme = {
+      name = "Catppuccin-Mocha-Compact-Mauve-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        variant = "mocha";
+        accents = ["mauve"];
+        size = "compact";
+      };
+    };
+
+    gtk3.extraCss = ''
+      .nautilus-list-view listview row { margin: 0px; padding: 0; }
+      .nautilus-list-view #NautilusViewCell { padding: 0px; }
+    '';
+    gtk4.extraCss = ''
+      .nautilus-list-view listview row { margin: 0px; padding: 0; }
+      .nautilus-list-view #NautilusViewCell { padding: 0px; }
+    '';
+  };
+
+  home.sessionVariables = {
+    # GDK_SCALE = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    # QT_FONT_DPI = "1";
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      document-font-name = "${globalFonts.sansSerif} 10";
+      monospace-font-name = "${globalFonts.monospace} 10";
+      # font-antialiasing = "rgba";
+      # font-hinting = "full";
+    };
+  };
+
+  programs.bat.config.theme = "Catppuccin-mocha";
+
+#   xdg.configFile."bat/themes/Catppuccin-mocha.tmTheme".source = pkgs.fetchurl {
+#     url = "https://raw.githubusercontent.com/catppuccin/bat/main/themes/Catppuccin%20Mocha.tmTheme";
+#     sha256 = "a8c40d2466489a68ebab3cbb222124639221bcf00c669ab45bab242cbb2783fc";
+#   };
+}
