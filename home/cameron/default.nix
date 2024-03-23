c42fcef6069d0532babd529dcb39d98eb372f017
TheBananaThief <cameron.salomone@gmail.com>
2024-03-22 20:23:18 -0400

    2024-03-22 20:23:18 EDT

--- home/cameron/default.nix
+++ home/cameron/default.nix
@@ -45,7 +45,7 @@
     # GDK_BACKEND = "wayland,x11";
     # GDK_SCALE = "1";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
 
     # EXPERIMENTAL: breaks some electron apps
     # Also makes a lot of electron apps use wayland
