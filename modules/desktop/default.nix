1c7d22de113bbb23b7ef251fb7fc605f65174141
TheBananaThief <cameron.salomone@gmail.com>
2024-03-26 17:19:21 -0400

    2024-03-26 17:19:21 EDT

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -8,25 +8,25 @@
     ../theme
   ];
 
-  # services.xserver = {
-  #   enable = true;
-  #   xkb.layout = "us";
-  #   xkb.variant = "";
-  #
-  #   # excludePackages = [ pkgs.xterm ];
-  #
-  #   libinput = {
-  #     enable = true;
-  #     touchpad = {
-  #       accelProfile = "flat";
-  #       accelSpeed = "0.0";
-  #       naturalScrolling = false;
-  #     };
-  #     mouse = {
-  #       accelProfile = "flat";
-  #       accelSpeed = "0.0";
-  #       naturalScrolling = false;
-  #     };
-  #   };
-  # };
+  services.xserver = {
+    enable = true;
+    xkb.layout = "us";
+    xkb.variant = "";
+
+    excludePackages = [ pkgs.xterm ];
+
+    libinput = {
+      enable = true;
+      touchpad = {
+        accelProfile = "flat";
+        accelSpeed = "0.0";
+        naturalScrolling = false;
+      };
+      mouse = {
+        accelProfile = "flat";
+        accelSpeed = "0.0";
+        naturalScrolling = false;
+      };
+    };
+  };
 }
