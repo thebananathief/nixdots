2ca38e92b2b30f44e0e55ed208b567cba07cb107
TheBananaThief <cameron.salomone@gmail.com>
2024-03-26 18:17:13 -0400

    2024-03-26 18:17:13 EDT gen 102

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -5,15 +5,17 @@
 
   services.xserver = {
     displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
 #    displayManager.defaultSession = "plasmawayland";
-    desktopManager.plasma5.enable = true;
+    # desktopManager.plasma5.enable = true;
   };
+  services.desktopManager.plasma6.enable = true;
 
-  environment.systemPackages = (with pkgs; [
-    
-  ]) ++ (with pkgs.libsForQt5; [
-
-  ]);
+  # environment.systemPackages = (with pkgs; [
+  #   
+  # ]) ++ (with pkgs.libsForQt5; [
+  #
+  # ]);
 
 #  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
 #    elisa
@@ -25,4 +27,10 @@
 #    plasma-browser-integration
 #    print-manager
 #  ];
+
+  # environment.plasma6.excludePackages = with pkgs.kdePackages; [
+  #   plasma-browser-integration
+  #   konsole
+  #   oxygen
+  # ];
 }
