94980261baa40dc150134a862cf11a933da49c51
TheBananaThief <cameron.salomone@gmail.com>
2024-04-17 23:03:21 -0400

    2024-04-17 23:03:21 EDT

--- modules/monero.nix
+++ modules/monero.nix
@@ -1,30 +1,4 @@
-{ pkgs, lib, ... }: let
-  json = pkgs.formats.json {};
-  configFile = json.generate "config.json" {
-    autosave = true;
-    cpu = true;
-    opencl = false;
-    cuda = true;
-    # randomx.igb-pages = true;
-    pools = {
-      # algo = "null";
-      # coin = "null";
-      url = "pool.supportxmr.com:443";
-      user = "455dygYZmT4Y3wgnqQc8qgXGcCW9vJevKcpKageR65dBYr5bndKP5GjFCvCownGN8dHVq9k12etDbNjvnTGXGhQ2EA6d8A6";
-      pass = "crazymonkey";
-      # rig-id = "null";
-      # nicehash = false;
-      keepalive = true;
-      # enabled = true;
-      tls = true;
-      # tls-fingerprint = "null";
-      # daemon = false;
-      # socks5 = "null";
-      # self-select = null;
-      # submit-to-origin = false;
-    }; 
-  };
-in {
+{ pkgs, lib, ... }: {
   # services.monero = {
   #   enable = true;
   #   mining = {
@@ -81,7 +55,33 @@ in {
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     description = "XMRig Mining Software Service";
-    serviceConfig = {
+    serviceConfig = let
+      json = pkgs.formats.json {};
+      configFile = json.generate "config.json" {
+        autosave = true;
+        cpu = true;
+        opencl = false;
+        cuda = true;
+        # randomx.igb-pages = true;
+        pools = {
+          # algo = "null";
+          # coin = "null";
+          url = "pool.supportxmr.com:443";
+          user = "455dygYZmT4Y3wgnqQc8qgXGcCW9vJevKcpKageR65dBYr5bndKP5GjFCvCownGN8dHVq9k12etDbNjvnTGXGhQ2EA6d8A6";
+          pass = "crazymonkey";
+          # rig-id = "null";
+          # nicehash = false;
+          keepalive = true;
+          # enabled = true;
+          tls = true;
+          # tls-fingerprint = "null";
+          # daemon = false;
+          # socks5 = "null";
+          # self-select = null;
+          # submit-to-origin = false;
+        }; 
+      };
+    in {
       ExecStartPre = "${pkgs.xmrig}/bin/xmrig --config=${configFile} --dry-run";
       ExecStart = "${pkgs.xmrig}/bin/xmrig --config=${configFile}";
       # https://xmrig.com/docs/miner/randomx-optimization-guide/msr
