f028b1d0910a0a89abd82a679faac9d7374fafe2
TheBananaThief <cameron.salomone@gmail.com>
2024-04-30 23:33:34 -0400

    2024-04-30 23:33:34 EDT

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -14,11 +14,11 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    # powerManagement.enable = true;
+    powerManagement.enable = true;
     
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    # powerManagement.finegrained = false;
+    powerManagement.finegrained = false;
     
     # Use the open source version of the kernel module (for driver 515.43.04+)
     open = false;
