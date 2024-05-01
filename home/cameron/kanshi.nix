f54592d7c49bba3d3c28eedbaa1c84d8ff3ac10f
TheBananaThief <cameron.salomone@gmail.com>
2024-04-30 23:09:11 -0400

    2024-04-30 23:09:11 EDT

--- home/cameron/kanshi.nix
+++ home/cameron/kanshi.nix
@@ -4,7 +4,7 @@
     enable = true;
     # Required for hyprland (kanshi is originally for sway)
     # systemdTarget = lib.mkIf config.wayland.windowManager.hyprland.enable "hyprland-session.target";
-    systemdTarget = "hyprland-session.target";
+    # systemdTarget = "hyprland-session.target";
 
     profiles = {
       desktop = {
@@ -33,7 +33,7 @@
           {
             criteria = "Lenovo Group Limited D27-30 URHHM364";
             mode = "1920x1080@60";
-            position = "1920,240";
+            position = "1920,-500";
             transform = "90";
             scale = 1.0;
             adaptiveSync = false;
