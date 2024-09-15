bee81d712f7e259472b3a9c83dad736142ac09d2
thebananathief <cameron.salomone@gmail.com>
2024-09-14 22:47:58 -0400

    2024-09-14 22:47:58 EDT

---
+++ hosts/talos/services/whoogle.nix
@@ -0,0 +1,16 @@
+{ config, ... }:
+let
+  cfg = config.myOptions.containers;
+  inherit (config.sops) secrets;
+in {
+  virtualisation.oci-containers.containers = {
+    whoogle = {
+      image = "benbusby/whoogle-search:latest";
+      ports = ["8050:5000"];
+      environment = {
+        WHOOGLE_USER = "cameron";
+        WHOOGLE_PASS = "dumbpassword";
+      } // cfg.common_env;
+    };
+  };
+}
