6ee0dde8617050acb157048319e21f68c87f9cbe
thebananathief <cameron.salomone@gmail.com>
2024-09-30 15:46:05 -0400

    2024-09-30 15:46:05 EDT gen 389

--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -1,4 +1,6 @@
-{ pkgs, ... }: {
+{ pkgs, nixos-cosmic, ... }: 
+{
+  imports = [ nixos-cosmic.nixosModules.default ];
   services = {
     desktopManager.cosmic.enable = true;
     displayManager.cosmic-greeter.enable = true;
