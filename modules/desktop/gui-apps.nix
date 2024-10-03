19dd6f1fbcb2c2a2648647a01039542c2fd01e8c
thebananathief <cameron.salomone@gmail.com>
2024-10-03 12:54:16 -0400

    changes from gargantuan-gen404 on 2024-10-03 12:54:16 EDT

--- modules/desktop/gui-apps.nix
+++ modules/desktop/gui-apps.nix
@@ -27,25 +27,25 @@
     # jetbrains.goland
   ];
 
-  systemd.services.tailscale-systray = {
-    description = "Tailscale system tray icon";
-    wantedBy = [ "graphical.target" ];
-    after = [ "tailscaled.service" ];
-    wants = [ "tailscaled.service" ];
-    path = [
-      pkgs.tailscale-systray
-    ];
-    environment = {
-      DISPLAY = "0";
-    };
-    startLimitBurst = 5;
-    startLimitIntervalSec = 20;
-    serviceConfig = {
-      Type = "simple";
-      User = "cameron";
-      ExecStart = "${pkgs.tailscale-systray}/bin/tailscale-systray";
-      Restart = "always";
-      RestartSec = 1;
-    };
-  };
+  # systemd.services.tailscale-systray = {
+  #   description = "Tailscale system tray icon";
+  #   wantedBy = [ "graphical.target" ];
+  #   after = [ "tailscaled.service" ];
+  #   wants = [ "tailscaled.service" ];
+  #   path = [
+  #     pkgs.tailscale-systray
+  #   ];
+  #   environment = {
+  #     DISPLAY = "0";
+  #   };
+  #   startLimitBurst = 5;
+  #   startLimitIntervalSec = 20;
+  #   serviceConfig = {
+  #     Type = "simple";
+  #     User = "cameron";
+  #     ExecStart = "${pkgs.tailscale-systray}/bin/tailscale-systray";
+  #     Restart = "always";
+  #     RestartSec = 1;
+  #   };
+  # };
 }
