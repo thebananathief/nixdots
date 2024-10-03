eb2587a233068afdd2ff794baeb72fadae142500
thebananathief <cameron.salomone@gmail.com>
2024-10-03 13:52:16 -0400

    changes from gargantuan-gen407 on 2024-10-03 13:52:16 EDT

--- modules/test-containers.nix
+++ modules/test-containers.nix
@@ -1,30 +1,30 @@
 {pkgs, config, ...}: {
-  virtualisation = {
-    # oci-containers.backend = "docker";
-    # docker = {
-    #   enable = true;
-    #   autoPrune.enable = true;
-    #   autoPrune.flags = [
-    #     "--all"
-    #   ];
-    # };
-    oci-containers.backend = "podman";
-    podman = {
-      enable = true;
-      dockerCompat = true;
-    #   dockerSocket.enable = true; # UNSAFE: This allows anyone in the "podman" group to gain root access - It also allows containers to do a bunch of stuff
-      defaultNetwork.settings.dns_enabled = true;
-      autoPrune.enable = true;
-      autoPrune.flags = [
-        "--all"
-      ];
-    };
-  };
+  # virtualisation = {
+  #   # oci-containers.backend = "docker";
+  #   # docker = {
+  #   #   enable = true;
+  #   #   autoPrune.enable = true;
+  #   #   autoPrune.flags = [
+  #   #     "--all"
+  #   #   ];
+  #   # };
+  #   oci-containers.backend = "podman";
+  #   podman = {
+  #     enable = true;
+  #     dockerCompat = true;
+  #   #   dockerSocket.enable = true; # UNSAFE: This allows anyone in the "podman" group to gain root access - It also allows containers to do a bunch of stuff
+  #     defaultNetwork.settings.dns_enabled = true;
+  #     autoPrune.enable = true;
+  #     autoPrune.flags = [
+  #       "--all"
+  #     ];
+  #   };
+  # };
 
   environment.systemPackages = with pkgs; [
-    podman-compose
-    dive
-    podman-tui
+    # podman-compose
+    # dive
+    # podman-tui
   ];
 
   # systemd.services.podman-create-pod-testdb = {
@@ -38,28 +38,46 @@
   #   '';
   # };
 
-  virtualisation.oci-containers.containers = {
-    mysql_source = {
-      image = "mysql:latest";
-      ports = [ "3306:3306" ];
-      # extraOptions = [ "--pod=testdb" ];
-      environment = {
-        MYSQL_RANDOM_ROOT_PASSWORD = "yes";
-        MYSQL_DATABASE = "skynet";
-        MYSQL_USER = "cameron";
-        MYSQL_PASSWORD = "dumbpassword";
+  # virtualisation.oci-containers.containers = {
+  #   mysql_source = {
+  #     image = "mysql:latest";
+  #     ports = [ "3306:3306" ];
+  #     # extraOptions = [ "--pod=testdb" ];
+  #     environment = {
+  #       MYSQL_RANDOM_ROOT_PASSWORD = "yes";
+  #       MYSQL_DATABASE = "skynet";
+  #       MYSQL_USER = "cameron";
+  #       MYSQL_PASSWORD = "dumbpassword";
+  #     };
+  #   };
+  #   mysql_target = {
+  #     image = "mysql:latest";
+  #     ports = [ "3307:3306" ];
+  #     # extraOptions = [ "--pod=testdb" ];
+  #     environment = {
+  #       MYSQL_RANDOM_ROOT_PASSWORD = "yes";
+  #       MYSQL_DATABASE = "skynet";
+  #       MYSQL_USER = "cameron";
+  #       MYSQL_PASSWORD = "dumbpassword";
+  #     };
+  #   };
+  # };
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+    initialDatabases = {
+      skynet.name = "skynet";
+      skynet.schema = "skynet_pkg";
+    };
+    ensureUsers = {
+      cameron.name = "cameron";
+      cameron.ensurePermissions = {
+        "*.*" = "ALL PRIVILEGES";
       };
     };
-    mysql_target = {
-      image = "mysql:latest";
-      ports = [ "3307:3306" ];
-      # extraOptions = [ "--pod=testdb" ];
-      environment = {
-        MYSQL_RANDOM_ROOT_PASSWORD = "yes";
-        MYSQL_DATABASE = "skynet";
-        MYSQL_USER = "cameron";
-        MYSQL_PASSWORD = "dumbpassword";
-      };
+    settings = {
+
     };
   };
 }
