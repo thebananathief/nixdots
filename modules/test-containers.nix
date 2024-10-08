ecf19736c93ecaa6c75d0bcea45a56abc813e3fc
thebananathief <cameron.salomone@gmail.com>
2024-10-08 14:50:22 -0400

    changes from gargantuan-gen416 on 2024-10-08 14:50:22 EDT

--- modules/test-containers.nix
+++ modules/test-containers.nix
@@ -108,7 +108,7 @@
       networking = {
         firewall = {
           enable = true;
-          allowedTCPPorts = [ 80 ];
+          allowedTCPPorts = [ 3307 ];
         };
         # Use systemd-resolved inside the container
         # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
