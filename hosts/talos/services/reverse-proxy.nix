272911ff81c5ef1630ba7705054d190758e525ad
thebananathief <cameron.salomone@gmail.com>
2024-11-13 20:09:23 -0500

    2024-11-13 20:09:23 EST

--- hosts/talos/services/reverse-proxy.nix
+++ hosts/talos/services/reverse-proxy.nix
@@ -21,9 +21,12 @@ in {
   services.caddy = {
     enable = true;
     email = useremail;
-    # globalConfig = ''
-    #   acme_dns cloudflare /run/secrets/cloudflare_api
-    # '';
+    package = (pkgs.callPackage ../../packages/custom-caddy.nix {
+      plugins = [ "github.com/caddy-dns/cloudflare" ];
+    });
+    globalConfig = ''
+      acme_dns cloudflare /run/secrets/cloudflare_api
+    '';
       # acme_dns cloudflare {env.CLOUDFLARE_DNS_API_TOKEN}
     # virtualHosts = {
     #   # Static fileserver and interactive filebrowser
