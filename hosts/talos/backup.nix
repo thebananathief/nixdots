17d27b0089df841cee6f20577b5f9b55953274c0
thebananathief <cameron.salomone@gmail.com>
2024-11-21 02:46:00 -0500

    Auto-commit from THOTH at 2024-11-21 02:46:00

--- hosts/talos/backup.nix
+++ hosts/talos/backup.nix
@@ -29,7 +29,7 @@ in {
         Persistent = true;
       };
       user = "restic";
-      repository = "sftp://restic@icebox:22//srv/restic-repo";
+      repository = "sftp://restic@icebox.localdomain:22//backups/talos";
       initialize = false;
       passwordFile = secrets.restic_talos_backup.path;
       paths = [ "/mnt/storage/media/family" ];
