691e6037d0bbd9d8cb92f8a51a477d52573498aa
thebananathief <cameron.salomone@gmail.com>
2024-11-27 11:45:27 -0500

    Auto-commit from THOTH at 2024-11-27 11:45:27

--- flake.nix
+++ flake.nix
@@ -28,8 +28,8 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
-    nixos-wsl.url = "github:nix-community/NixOS-WSL";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    # nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    # nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
     
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
