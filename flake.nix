39570ca06c06e2e133b46d41d61d6ae87f094642
thebananathief <cameron.salomone@gmail.com>
2024-11-27 11:08:25 -0500

    Auto-commit from THOTH at 2024-11-27 11:08:25

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
         config.allowUnfree = true;
         # config.permittedInsecurePackages = [ 
         # ];
-    })
+    });
 
     argDefaults = {
       inherit username useremail globalFonts;
