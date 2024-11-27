0529117644ad1425f3714129f757162ee1c4a130
thebananathief <cameron.salomone@gmail.com>
2024-11-27 13:28:00 -0500

    Auto-commit from THOTH at 2024-11-27 13:28:00

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
     # This line enables you to import the inputs (flakes/modules from github) into modules, aka: ( nixos-cosmic, sops-nix, ... ): {}
     } // inputs;
 
-    nixosSystem = {
+    nixosSystem = import {
       system ? "x86_64-linux",
       nixos-modules,
       home-module,
