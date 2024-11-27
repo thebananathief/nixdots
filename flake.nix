02b1977eb86dbff375b5505b972803e2c85abb22
thebananathief <cameron.salomone@gmail.com>
2024-11-27 11:43:12 -0500

    Auto-commit from THOTH at 2024-11-27 11:43:12

--- flake.nix
+++ flake.nix
@@ -79,7 +79,7 @@
       nixos-modules,
       home-module
     }: let
-      specialArgs = { inherit argDefaults; } // args;
+      specialArgs = argDefaults // args;
     in nixpkgs.lib.nixosSystem {
         inherit system specialArgs;
         pkgs = nixpkgsCustom system;
