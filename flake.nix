d7b92b16fc53e3a3cc7905f8ea608910551685ca
thebananathief <cameron.salomone@gmail.com>
2024-11-27 12:20:10 -0500

    Auto-commit from THOTH at 2024-11-27 12:20:10

--- flake.nix
+++ flake.nix
@@ -73,27 +73,28 @@
     } // inputs;
 
     # Function to declare NixOS systems with home manager configs
-    nixosSystem = {
-      system ? "x86_64-linux",
-      args ? {},
-      nixos-modules,
-      home-module
-    }: let
-      specialArgs = argDefaults // args;
-    in nixpkgs.lib.nixosSystem {
-        inherit system specialArgs;
-        pkgs = nixpkgsCustom system;
-        modules = nixos-modules ++ [
-          ../modules/common
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "hm-backup";
-            home-manager.extraSpecialArgs = specialArgs;
-            home-manager.users."${username}" = home-module;
-          }
-        ];
-      };
+    nixosSystem = import ./nixosSystem.nix;
+    # nixosSystem = {
+    #   system ? "x86_64-linux",
+    #   args ? {},
+    #   nixos-modules,
+    #   home-module
+    # }: let
+    #   specialArgs = argDefaults // args;
+    # in nixpkgs.lib.nixosSystem {
+    #   inherit system specialArgs;
+    #   pkgs = nixpkgsCustom system;
+    #   modules = nixos-modules ++ [
+    #     ../modules/common
+    #     home-manager.nixosModules.home-manager {
+    #       home-manager.useGlobalPkgs = true;
+    #       home-manager.useUserPackages = true;
+    #       home-manager.backupFileExtension = "hm-backup";
+    #       home-manager.extraSpecialArgs = specialArgs;
+    #       home-manager.users."${username}" = home-module;
+    #     }
+    #   ];
+    # };
   in {
     nixosConfigurations = {
       # talos = nixosSystem {
---
+++ nixosSystem.nix
@@ -0,0 +1,21 @@
+{
+  system ? "x86_64-linux",
+  args ? {},
+  nixos-modules,
+  home-module
+}: let
+  specialArgs = argDefaults // args;
+in nixpkgs.lib.nixosSystem {
+  inherit system specialArgs;
+  pkgs = nixpkgsCustom system;
+  modules = nixos-modules ++ [
+    ../modules/common
+    home-manager.nixosModules.home-manager {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.backupFileExtension = "hm-backup";
+      home-manager.extraSpecialArgs = specialArgs;
+      home-manager.users."${username}" = home-module;
+    }
+  ];
+};
