fa3579b87516efd032d8e6360ab395bcc31b5bcf
thebananathief <cameron.salomone@gmail.com>
2024-11-27 19:50:26 -0500

    changes from thoth-wsl on 2024-11-27 19:50:26 EST

--- hosts/thoth/wsl-default.nix
+++ hosts/thoth/wsl-default.nix
@@ -11,23 +11,11 @@ in {
     firewall.enable = false;
   };
 
-  nix = {
-    nixPath = [
-      "nixpkgs=${nixpkgs.outPath}"
-      "nixos-config=/home/cameron/nixdots/flake.nix"
-      "/nix/var/nix/profiles/per-user/root/channels"
-    ];
-
-    # registry = {
-    #   nixpkgs = {
-    #     flake = nixpkgs;
-    #   };
-    # };
-
-    # package = pkgs.nixFlakes;
-  };
-
-  # environment.enableAllTerminfo = true;
+  nix.nixPath = [
+    "nixpkgs=${nixpkgs.outPath}"
+    "nixos-config=/home/cameron/nixdots/flake.nix"
+    "/nix/var/nix/profiles/per-user/root/channels"
+  ];
 
   security.sudo.wheelNeedsPassword = false;
   sops = {
@@ -72,7 +60,6 @@ in {
     wslConf.interop.appendWindowsPath = true;
     wslConf.network.generateHosts = true;
     wslConf.network.generateResolvConf = true;
-    # wslConf.user = "root";
     defaultUser = username;
     startMenuLaunchers = true;
 
@@ -85,25 +72,7 @@ in {
   #   enableOnBoot = true;
   #   autoPrune.enable = true;
   # };
-  
-  # FIXME: uncomment the next block to make vscode running in Windows "just work" with NixOS on WSL
-  # solution adapted from: https://github.com/K900/vscode-remote-workaround
-  # more information: https://github.com/nix-community/NixOS-WSL/issues/238 and https://github.com/nix-community/NixOS-WSL/issues/294
-  # systemd.user = {
-  #   paths.vscode-remote-workaround = {
-  #     wantedBy = ["default.target"];
-  #     pathConfig.PathChanged = "%h/.vscode-server/bin";
-  #   };
-  #   services.vscode-remote-workaround.script = ''
-  #     for i in ~/.vscode-server/bin/*; do
-  #       if [ -e $i/node ]; then
-  #         echo "Fixing vscode-server in $i..."
-  #         ln -sf ${pkgs.nodejs_18}/bin/node $i/node
-  #       fi
-  #     done
-  #   '';
-  # };
-  
+
   
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
