3b904a17b838375c90576684a2ca5353671bce9b
thebananathief <cameron.salomone@gmail.com>
2024-12-15 10:53:16 -0500

    changes from thoth-wsl-gen on 2024-12-15 10:53:16 EST

--- hosts/talos/services/starbase/default.nix
+++ hosts/talos/services/starbase/default.nix
@@ -3,6 +3,14 @@ let
   cfg = config.mediaServer;
   inherit (config.sops) secrets;
 in {
+  users = {
+    groups.starbase = {};
+    users.starbase = {
+      group = "starbase";
+      isSystemUser = true;
+    };
+  };
+
   virtualisation.oci-containers.containers = {
     starbase80 = {
       image = "jordanroher/starbase-80:latest";
@@ -13,12 +21,18 @@ in {
         "${ cfg.dataDir }/starbase/public/icons:/app/public/icons"
       ];
       ports = [ "8002:4173" ];
+      user = "starbase:starbase";
       environment = {
         TITLE = "Talos Home";
         LOGO = "/starbase80.jpg";
       };
     };
   };
+  
+  systemd.tmpfiles.rules = [
+    "d ${ cfg.dataDir }/starbase 0755 starbase starbase - -"
+    "d ${ cfg.dataDir }/starbase/public 0755 starbase starbase - -"
+  ];
   
   services.caddy.virtualHosts = {
     "base.${ config.networking.fqdn }".extraConfig = ''
