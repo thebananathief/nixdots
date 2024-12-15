a20be13b83b82686268c6f6abd0340220521d533
thebananathief <cameron.salomone@gmail.com>
2024-12-15 11:44:06 -0500

    changes from thoth-wsl-gen on 2024-12-15 11:44:06 EST

--- hosts/talos/services/starbase/default.nix
+++ hosts/talos/services/starbase/default.nix
@@ -2,11 +2,61 @@
 let
   cfg = config.mediaServer;
   inherit (config.sops) secrets;
+  configFile = builtins.toJSON [
+    {
+      category = "Services";
+      services = [
+        {
+          name = "Archivebox";
+          uri = "https://archivebox.mywebsite.com";
+          description = "Backup webpages";
+          icon = "/icons/archivebox.jpg";
+        }
+        {
+          name = "Authelia";
+          uri = "https://auth.mywebsite.com";
+          description = "Authentication";
+          icon = "/icons/authelia.png";
+        }
+        {
+          name = "Calibre";
+          uri = "https://calibre.mywebsite.com";
+          description = "eBook library";
+          icon = "/icons/calibre.png";
+        }
+      ];
+    }
+    {
+      category = "Devices";
+      bubble = true;
+      services = [
+        {
+          name = "Router";
+          uri = "http://192.168.1.1/";
+          description = "Netgear Orbi";
+          icon = "/icons/router.png";
+        }
+        {
+          name = "Home Assistant";
+          uri = "http://homeassistant.local:8123/";
+          description = "Home automation";
+          icon = "home-assistant";
+          iconBubble = false;
+        }
+        {
+          name = "Synology";
+          uri = "http://synology:5000";
+          description = "Network storage";
+          icon = "/icons/synology.png";
+        }
+      ];
+    }
+  ];
 in {
   users = {
-    groups.starbase = { gid = 983; };
+    groups.starbase = { gid = 970; };
     users.starbase = {
-      uid = 984;
+      uid = 980;
       group = "starbase";
       isSystemUser = true;
     };
@@ -22,7 +72,7 @@ in {
         "${ cfg.dataDir }/starbase/public/icons:/app/public/icons"
       ];
       ports = [ "8002:4173" ];
-      user = "984:983";
+      user = "980:970";
       environment = {
         TITLE = "Talos Home";
         LOGO = "/starbase80.jpg";
@@ -30,117 +80,17 @@ in {
     };
   };
 
-  environment.etc."/appdata/starbase/config.json" = {
-    text = builtins.toJSON [
-      {
-        category = "Services";
-        services = [
-          {
-            name = "Archivebox";
-            uri = "https://archivebox.mywebsite.com";
-            description = "Backup webpages";
-            icon = "/icons/archivebox.jpg";
-          }
-          {
-            name = "Authelia";
-            uri = "https://auth.mywebsite.com";
-            description = "Authentication";
-            icon = "/icons/authelia.png";
-          }
-          {
-            name = "Calibre";
-            uri = "https://calibre.mywebsite.com";
-            description = "eBook library";
-            icon = "/icons/calibre.png";
-          }
-        ];
-      }
-      {
-        category = "Devices";
-        bubble = true;
-        services = [
-          {
-            name = "Router";
-            uri = "http://192.168.1.1/";
-            description = "Netgear Orbi";
-            icon = "/icons/router.png";
-          }
-          {
-            name = "Home Assistant";
-            uri = "http://homeassistant.local:8123/";
-            description = "Home automation";
-            icon = "home-assistant";
-            iconBubble = false;
-          }
-          {
-            name = "Synology";
-            uri = "http://synology:5000";
-            description = "Network storage";
-            icon = "/icons/synology.png";
-          }
-        ];
-      }
-    ];
-    mode = "0755";
-  };
-
-#   `/appdata/starbase/config.json` = ''
-# [
-# 	{
-# 		"category": "Services",
-# 		"services": [
-# 			{
-# 				"name": "Archivebox",
-# 				"uri": "https://archivebox.mywebsite.com",
-# 				"description": "Backup webpages",
-# 				"icon": "/icons/archivebox.jpg"
-# 			},
-# 			{
-# 				"name": "Authelia",
-# 				"uri": "https://auth.mywebsite.com",
-# 				"description": "Authentication",
-# 				"icon": "/icons/authelia.png"
-# 			},
-# 			{
-# 				"name": "Calibre",
-# 				"uri": "https://calibre.mywebsite.com",
-# 				"description": "eBook library",
-# 				"icon": "/icons/calibre.png"
-# 			}
-# 		]
-# 	},
-# 	{
-# 		"category": "Devices",
-# 		"bubble": true,
-# 		"services": [
-# 			{
-# 				"name": "Router",
-# 				"uri": "http://192.168.1.1/",
-# 				"description": "Netgear Orbi",
-# 				"icon": "/icons/router.png"
-# 			},
-# 			{
-# 				"name": "Home Assistant",
-# 				"uri": "http://homeassistant.local:8123/",
-# 				"description": "Home automation",
-# 				"icon": "home-assistant",
-# 				"iconBubble": false
-# 			},
-# 			{
-# 				"name": "Synology",
-# 				"uri": "http://synology:5000",
-# 				"description": "Network storage",
-# 				"icon": "/icons/synology.png"
-# 			}
-# 		]
-# 	}
-# ]
-#   '';
+  system.activationScripts.starbaseSetup = ''
+    mkdir -p ${ cfg.dataDir }/starbase/public/icons
+    echo '${configFile}' > ${ cfg.dataDir }/starbase/config.json
+    chown -R starbase:starbase ${ cfg.dataDir }/starbase
+    chmod -R 750 ${ cfg.dataDir }/starbase
+  '';
   
-  systemd.tmpfiles.rules = [
-    "d ${ cfg.dataDir }/starbase 0755 starbase starbase - -"
-    "d ${ cfg.dataDir }/starbase/public 0755 starbase starbase - -"
-  ];
+  # systemd.tmpfiles.rules = [
+  #   "d ${ cfg.dataDir }/starbase 0755 starbase starbase - -"
+  #   "d ${ cfg.dataDir }/starbase/public 0755 starbase starbase - -"
+  # ];
   
   services.caddy.virtualHosts = {
     "base.${ config.networking.fqdn }".extraConfig = ''
