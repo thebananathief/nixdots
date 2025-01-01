847d54960596af7c3c06fd9f65456687b7964127
thebananathief <cameron.salomone@gmail.com>
2025-01-01 13:12:48 -0500

    changes from thoth-wsl-gen on 2025-01-01 13:12:48 EST

--- hosts/talos/monitoring.nix
+++ hosts/talos/monitoring.nix
@@ -31,8 +31,8 @@ in {
             targets = [
               "localhost:9100"
               "localhost:9633"
-              # "localhost:49090"
-              "localhost:9436"
+              "localhost:49090" # docker mktxp exporter
+              # "localhost:9436" # crappy mikrotik exporter
               # "localhost:9753"
             ];
           }
@@ -51,6 +51,19 @@ in {
           }
         ];
       }
+      {
+        job_name = "icebox2";
+        scrape_interval = "1m";
+        scrape_timeout = "10s";
+        static_configs = [
+          {
+            targets = [
+              "icebox2:9100"
+              "icebox2:9633"
+            ];
+          }
+        ];
+      }
     ];
     exporters = {
       node = {
@@ -60,34 +73,36 @@ in {
         enable = true;
         maxInterval = "60s";
       };
-      mikrotik = {
-        enable = true;
-        configuration = {
-          devices = [
-            {
-              name = "router";
-              address = "192.168.0.1";
-              user = "prometheus";
-              password = "changeme";
-            }
-            {
-              name = "ap";
-              address = "192.168.0.50";
-              user = "prometheus";
-              password = "changeme";
-            }
-          ];
-          features = {
-            bgp = true;
-            dhcp = true;
-            dhcpv6 = true;
-            dhcpl = true;
-            routes = true;
-            pools = true;
-            optics = true;
-          };
-        };
-      };
+
+      # mikrotik = {
+      #   enable = true;
+      #   configuration = {
+      #     devices = [
+      #       {
+      #         name = "router";
+      #         address = "192.168.0.1";
+      #         user = "prometheus";
+      #         password = "changeme";
+      #       }
+      #       {
+      #         name = "ap";
+      #         address = "192.168.0.50";
+      #         user = "prometheus";
+      #         password = "changeme";
+      #       }
+      #     ];
+      #     features = {
+      #       bgp = true;
+      #       dhcp = true;
+      #       dhcpv6 = true;
+      #       dhcpl = true;
+      #       routes = true;
+      #       pools = true;
+      #       optics = true;
+      #     };
+      #   };
+      # };
+
       # restic = {
       #   enable = true;
       #   environmentFile = pkgs.writeText "restic-exporter.env" ''
@@ -100,84 +115,85 @@ in {
   };
 
   # Docker container for mikrotik exporter
-  # virtualisation.oci-containers.containers = {
-  #   mktxp = let
-  #     mktxpConfig = pkgs.runCommand "mktxp.conf" {} ''
-  #       echo '${pkgs.lib.generators.toINI {} {
-  #         "Router" = {
-  #           hostname = "192.168.0.1";
-  #         };
-  #         "Access Point" = {
-  #           hostname = "192.168.0.50"; 
-  #         };
-  #         default = {
-  #           enabled = true;
-  #           hostname = "localhost";
-  #           port = 8728;
-  #           username = "prometheus";
-  #           password = "changeme";
-  #           use_ssl = false;
-  #           no_ssl_certificate = false;
-  #           ssl_certificate_verify = false;
-  #           plaintext_login = true;
-  #           installed_packages = true;
-  #           dhcp = true;
-  #           dhcp_lease = true;
-  #           connections = true;
-  #           connection_stats = false;
-  #           interface = true;
-  #           route = true;
-  #           pool = true;
-  #           firewall = true;
-  #           neighbor = true;
-  #           dns = false;
-  #           ipv6_route = false;
-  #           ipv6_pool = false;
-  #           ipv6_firewall = false;
-  #           ipv6_neighbor = false;
-  #           poe = true;
-  #           monitor = true;
-  #           netwatch = true;
-  #           public_ip = true;
-  #           wireless = true;
-  #           wireless_clients = true;
-  #           capsman = true;
-  #           capsman_clients = true;
-  #           eoip = false;
-  #           gre = false;
-  #           ipip = false;
-  #           lte = false;
-  #           ipsec = false;
-  #           switch_port = false;
-  #           kid_control_assigned = false;
-  #           kid_control_dynamic = false;
-  #           user = true;
-  #           queue = true;
-  #           bgp = false;
-  #           routing_stats = false;
-  #           certificate = false;
-  #           remote_dhcp_entry = null;
-  #           remote_capsman_entry = null;
-  #           use_comments_over_names = true;
-  #           check_for_updates = false;
-  #         };
-  #       }}' > $out/mktxp.conf
-  #       chmod 644 $out
-  #     '';
-  #     # mktxpConfig = pkgs.writeTextFile {
-  #     #   name = "mktxp.conf";
-  #     #   mode = "0644";
-  #     #   text = (pkgs.lib.generators.toINI {} {
-  #     #   });
-  #     # };
-  #   in {
-  #     image = "ghcr.io/akpw/mktxp:latest";
-  #     volumes = [
-  #       "${ mktxpConfig }:/home/mktxp/mktxp/mktxp.conf:ro"
-  #     ];
-  #     ports = [ "49090:49090" ];
-  #   };
-  # };
+  virtualisation.oci-containers.containers = {
+    mktxp = let
+      mktxpConfigFile = pkgs.lib.generators.toINI {} {
+        "Router" = {
+          hostname = "192.168.0.1";
+        };
+        "Access Point" = {
+          hostname = "192.168.0.50"; 
+        };
+        default = {
+          enabled = true;
+          hostname = "localhost";
+          port = 8728;
+          username = "prometheus";
+          password = "changeme";
+          use_ssl = false;
+          no_ssl_certificate = false;
+          ssl_certificate_verify = false;
+          plaintext_login = true;
+          installed_packages = true;
+          dhcp = true;
+          dhcp_lease = true;
+          connections = true;
+          connection_stats = false;
+          interface = true;
+          route = true;
+          pool = true;
+          firewall = true;
+          neighbor = true;
+          dns = false;
+          ipv6_route = false;
+          ipv6_pool = false;
+          ipv6_firewall = false;
+          ipv6_neighbor = false;
+          poe = true;
+          monitor = true;
+          netwatch = true;
+          public_ip = true;
+          wireless = true;
+          wireless_clients = true;
+          capsman = true;
+          capsman_clients = true;
+          eoip = false;
+          gre = false;
+          ipip = false;
+          lte = false;
+          ipsec = false;
+          switch_port = false;
+          kid_control_assigned = false;
+          kid_control_dynamic = false;
+          user = true;
+          queue = true;
+          bgp = false;
+          routing_stats = false;
+          certificate = false;
+          remote_dhcp_entry = null;
+          remote_capsman_entry = null;
+          use_comments_over_names = true;
+          check_for_updates = false;
+        };
+      };
+      mktxpConfig = pkgs.runCommand "mktxp" {} ''
+        cp ${mktxpConfigFile} $out/mktxp.conf
+        chmod 644 $out
+      '';
+      # mktxpConfig = pkgs.writeTextFile {
+      #   name = "mktxp.conf";
+      #   mode = "0644";
+      #   text = (pkgs.lib.generators.toINI {} {
+      #   });
+      # };
+    in {
+      image = "ghcr.io/akpw/mktxp:latest";
+      volumes = [
+        "${ mktxpConfig }:/home/mktxp/mktxp/"
+      ];
+      ports = [ "49090:49090" ];
+    };
+  };
 
   services.caddy.virtualHosts = {
     "grafana.${ config.networking.fqdn }".extraConfig = ''
@@ -188,7 +204,7 @@ in {
         reverse_proxy localhost:3000
       }
       handle {
-        respond "Unauthorized" 403
+        respond "Unauthorized" 403 
       }
     '';
   };
