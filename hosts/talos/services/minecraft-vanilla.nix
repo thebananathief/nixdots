bf19b1d620e7a29812b1736b1e2e7ee1e300ac60
thebananathief <cameron.salomone@gmail.com>
2025-02-17 19:58:33 -0500

    changes from thoth-wsl-gen on 2025-02-17 19:58:33 EST

--- hosts/talos/services/minecraft-vanilla.nix
+++ hosts/talos/services/minecraft-vanilla.nix
@@ -1,20 +1,20 @@
 { config, lib, pkgs, ... }:
 with lib;
 let 
-  serverPort = 25565;
-  rconPort = 25575;
 in {
   services.minecraft-server = {
     enable = true;
     eula = true;
+    openFirewall = true;
+    # package = ;
     serverProperties = {
       motd = "Kappa";
-      server-port = serverPort;
       difficulty = 3;
       gamemode = 0;
       max-players = 24;
       enable-rcon = false;
-      "rcon.port" = rconPort;
+      server-port = 25565;
+      "rcon.port" = 25575;
       "rcon.password" = "stupidpassword";
     };
   };
