0ef1c6baf3b3ebf6dbc56e8173c1aa50bcd21562
thebananathief <cameron.salomone@gmail.com>
2025-05-03 14:00:05 -0400

    changes from gargantuan-gen438 on 2025-05-03 14:00:05 EDT

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -22,7 +22,7 @@
       builders-use-substitutes = true;
       trusted-substituters = lib.mkBefore [
         "https://cache.nixos.org/?priority=5"
-        "https://nix-community.cachix.org?priority=10"
+        # "https://nix-community.cachix.org?priority=10"
         # "https://ai.cachix.org"
         # "https://anyrun.cachix.org"
         "https://cosmic.cachix.org"
@@ -30,7 +30,7 @@
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         # "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
         # "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
