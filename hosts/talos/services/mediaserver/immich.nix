a343cd775483c278cbce410401eecf7df5d718bb
thebananathief <cameron.salomone@gmail.com>
2025-05-15 23:24:01 -0400

    changes from talos-gen537 on 2025-05-15 23:24:01 EDT

--- hosts/talos/services/mediaserver/immich.nix
+++ hosts/talos/services/mediaserver/immich.nix
@@ -38,19 +38,19 @@ in {
   # redis on unix sock and postgresql at immich@localhost:5432
   services.immich = {
     enable = true;
-    host = "localhost";
+#     host = "localhost";
     # port = 2283;
     # openFirewall = true;
-    mediaLocation = "${cfg.storageDir}/media/family/immich-media";
+#     mediaLocation = "${cfg.storageDir}/media/family/immich-media";
     # secretsFile = secrets."postgres-immich.env".path;
-    environment = {
+#     environment = {
       # IMMICH_LOG_LEVEL = "verbose";
-      UPLOAD_LOCATION = "${cfg.storageDir}/media/family/uploads";
-    };
+#       UPLOAD_LOCATION = "${cfg.storageDir}/media/family/uploads";
+#     };
     accelerationDevices = [
       "/dev/dri/renderD128"
     ];
-    settings.server.externalDomain = "https://photos.${config.networking.publicDomain}";
+#     settings.server.externalDomain = "https://photos.${config.networking.publicDomain}";
   };
 
   services.caddy.virtualHosts = {
