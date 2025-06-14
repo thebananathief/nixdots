28d1af6f5163401006724ca516d06aded999a09e
thebananathief <cameron.salomone@gmail.com>
2025-06-14 00:34:12 -0400

    changes from thoth-wsl-gen on 2025-06-14 00:34:12 EDT

--- hosts/talos/services/qubic.nix
+++ hosts/talos/services/qubic.nix
@@ -2,25 +2,23 @@
   cfg = config.mediaServer;
   inherit (config.sops) secrets;
 
-  configFileContent = builtins.toJSON [
-    {
-      ClientSettings = {
-        poolAddress = "wss://wps.qubic.li/ws";
-        alias = "qli Machina man";
-        trainer = {
-          cpu = true;
-          gpu = false;
-          gpuVersion = "CUDA";
-          cpuVersion = "AVX512";
-          cpuThreads = 5;
-        };
-        pps = true;
-        accessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6Ijk2NDZkOTgzLWQ2OGQtNDBhMS1hMGZjLWE0MTMxM2FkODU1MiIsIk1pbmluZyI6IiIsIm5iZiI6MTc0OTg3Mjk2NywiZXhwIjoxNzgxNDA4OTY3LCJpYXQiOjE3NDk4NzI5NjcsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.Sop7jqZgpArESaSZSItWcTWvBUEQK-fdVmxk4r64naMPhi1pmyHdWQyF-IHWBYTowEIyH3cZXXaBtZqOS8ZiHQG1SZsalcjHoc_jfNM0fl6uBRsdpTmxEjzPdyuSAKtHP8ycepSt68F1GYpokArJe_YN1XUxOQez2SYbZRwXO4kNobq6Oz96ISnJMdkvo7bjJbiHtNIDya6_oKPSJa8_yHlwzuTWn6vf3WdXP6ZwT_er5BsYBWoTkS9UDLVca68P8fPUHlaRgEFRtNPCNezBXKKgEr2M1Py2k9U2sOBL0pAHuK9XCu472t5UN8USqnQ49UpIl8_bnnbibkkvBKzzcA";
-        qubicAddress = null;
-        idling = null;
+  configFileContent = builtins.toJSON {
+    ClientSettings = {
+      poolAddress = "wss://wps.qubic.li/ws";
+      alias = "qli Machina man";
+      trainer = {
+        cpu = true;
+        gpu = false;
+        gpuVersion = "CUDA";
+        cpuVersion = "AVX512";
+        cpuThreads = 5;
       };
-    }
-  ];
+      pps = true;
+      accessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6Ijk2NDZkOTgzLWQ2OGQtNDBhMS1hMGZjLWE0MTMxM2FkODU1MiIsIk1pbmluZyI6IiIsIm5iZiI6MTc0OTg3Mjk2NywiZXhwIjoxNzgxNDA4OTY3LCJpYXQiOjE3NDk4NzI5NjcsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.Sop7jqZgpArESaSZSItWcTWvBUEQK-fdVmxk4r64naMPhi1pmyHdWQyF-IHWBYTowEIyH3cZXXaBtZqOS8ZiHQG1SZsalcjHoc_jfNM0fl6uBRsdpTmxEjzPdyuSAKtHP8ycepSt68F1GYpokArJe_YN1XUxOQez2SYbZRwXO4kNobq6Oz96ISnJMdkvo7bjJbiHtNIDya6_oKPSJa8_yHlwzuTWn6vf3WdXP6ZwT_er5BsYBWoTkS9UDLVca68P8fPUHlaRgEFRtNPCNezBXKKgEr2M1Py2k9U2sOBL0pAHuK9XCu472t5UN8USqnQ49UpIl8_bnnbibkkvBKzzcA";
+      qubicAddress = null;
+      idling = null;
+    };
+  };
 
   qubicConfig = pkgs.writeText "appsettings.json" configFileContent;
 in {
