bbe987fc212b8fd451430434966ebd4ea336232e
Cameron Salomone <cameron.salomone@gmail.com>
2025-09-18 17:51:34 -0400

    changes from gargantuan on 2025-09-18 17:51:34 EDT

--- hosts/talos/services/homeassistant.nix
+++ hosts/talos/services/homeassistant.nix
@@ -11,7 +11,7 @@
         temperature_unit = "F";
         time_zone = "America/New_York";
       };
-      feedreader.urls = [ "https://nixos.org/blogs.xml" ];
+      # feedreader.urls = [ "https://nixos.org/blogs.xml" ];
     };
   };
 
