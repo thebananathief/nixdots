36adee118c93932ac5e116f9f5cdd9645bfb4ab9
thebananathief <cameron.salomone@gmail.com>
2025-11-06 20:54:44 -0500

    changes from thoth-wsl-gen on 2025-11-06 20:54:44 EST

--- hosts/talos/services/influxdb.nix
+++ hosts/talos/services/influxdb.nix
@@ -3,6 +3,7 @@
 in {
   sops.secrets = {
     influx_db_pass = {neededForUsers = true;};
+    influx_db_token = {neededForUsers = true;};
   };
 
   services.influxdb2 = {
@@ -11,24 +12,33 @@ in {
     provision = {
       enable = true;
 
-      users = {
-        cameron = {
-          passwordFile = secrets.influx_db_pass.path;
-          org = "myorg";
-          bucket = "mybucket";
-        };
+      initialSetup = {
+        username = "cameron";
+        passwordFile = secrets.influx_db_pass.path;
+        tokenFile = secrets.influx_db_token.path;
+        organization = "myorg";
+        bucket = "mybucket";
+        retention = 31536000; # 1 year
       };
 
-      organizations = {
-        myorg = {
-          description = "My organization";
-          buckets.mybucket = {
-            description = "My bucket";
-            retention = 31536000; # 1 year
-          };
-        };
-      }
-    }
+      # users = {
+      #   cameron = {
+      #     passwordFile = secrets.influx_db_pass.path;
+      #     org = "myorg";
+      #     bucket = "mybucket";
+      #   };
+      # };
+
+      # organizations = {
+      #   myorg = {
+      #     description = "My organization";
+      #     buckets.mybucket = {
+      #       description = "My bucket";
+      #       retention = 31536000; # 1 year
+      #     };
+      #   };
+      # };
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 8086 ];
