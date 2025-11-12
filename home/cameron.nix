cab7ad0972af353eb9eb9eb5cb5c09592b06cae0
thebananathief <cameron.salomone@gmail.com>
2025-11-11 22:07:10 -0500

    changes from thoth-wsl-gen34 on 2025-11-11 22:07:10 EST

--- home/cameron.nix
+++ home/cameron.nix
@@ -3,20 +3,7 @@
   lib,
   inputs,
   ...
-}: 
-let
-  plandex = pkgs.plandex.overrideAttrs (oldAttrs: rec {
-    inherit (oldAttrs) pname;
-    version = "2.2.1";
-    src = pkgs.fetchFromGitHub {
-      owner = "plandex-ai";
-      repo = "plandex";
-      rev = "cli/v${version}";
-      hash = "sha256-xtK/6eK3Xm7vGgADsVzFOKaFI7E2uYFu/E/NiyeLWhk=";
-    };
-  });
-in
-rec {
+}: rec {
   imports = [
     ./git.nix
     ./zsh.nix
@@ -27,9 +14,6 @@ rec {
   # home.homeDirectory = "/home/cameron";
 
   programs.opencode.enable = true;
-  home.packages = with pkgs; [
-    plandex
-  ];
 
   home.stateVersion = "23.05";
 }
