a00a2423763182de06f262c76bdd65417eec79cb
Cameron Salomone <cameron.salomone@gmail.com>
2025-11-22 17:19:23 -0500

    changes from gargantuan on 2025-11-22 17:19:23 EST

--- hosts/talos/services/hedgedoc.nix
+++ hosts/talos/services/hedgedoc.nix
@@ -1,34 +1,41 @@
 {config, ...}: let
-  cfg = config.mediaServer;
+
 in {
-  virtualisation.oci-containers.containers = {
-    hedgedoc = {
-      image = "lscr.io/linuxserver/hedgedoc:latest";
-      volumes = [
-        "${ cfg.dataDir }/hedgedoc:/config"
-      ];
-      ports = [ "8019:3000" ];
-      environment = {
-        # DB_HOST = "mysql";
-        # DB_PORT = 3306;
-        # DB_USER = "root";
-        # DB_PASS = "${ mysql_password }";
-        # DB_NAME = "hedgedoc";
-        CMD_CONFIG_FILE = ''
-        {
-          "dialect": "sqlite",
-          "storage": "/config/hedgedoc.sqlite"
+    services.hedgedoc = {
+        enable = true;
+        settings = {
+            domain = "hedgedoc.${config.networking.fqdn}";
+            port = 8019;
         }
-        '';
-        NODE_ENV = "production";
-        CMD_URL_ADDPORT = "true";
-        # CMD_DOMAIN = "notes.${ config.networking.fqdn }";
-        # CMD_PROTOCOL_USESSL = "false"; #optional - use if on a reverse proxy
-        # CMD_PORT = 3000; #optional
-        # CMD_ALLOW_ORIGIN = "['localhost']"; #optional
-      } // cfg.common_env;
     };
-  };
+#   virtualisation.oci-containers.containers = {
+#     hedgedoc = {
+#       image = "lscr.io/linuxserver/hedgedoc:latest";
+#       volumes = [
+#         "${ cfg.dataDir }/hedgedoc:/config"
+#       ];
+#       ports = [ "8019:3000" ];
+#       environment = {
+#         # DB_HOST = "mysql";
+#         # DB_PORT = 3306;
+#         # DB_USER = "root";
+#         # DB_PASS = "${ mysql_password }";
+#         # DB_NAME = "hedgedoc";
+#         CMD_CONFIG_FILE = ''
+#         {
+#           "dialect": "sqlite",
+#           "storage": "/config/hedgedoc.sqlite"
+#         }
+#         '';
+#         NODE_ENV = "production";
+#         CMD_URL_ADDPORT = "true";
+#         # CMD_DOMAIN = "notes.${ config.networking.fqdn }";
+#         # CMD_PROTOCOL_USESSL = "false"; #optional - use if on a reverse proxy
+#         # CMD_PORT = 3000; #optional
+#         # CMD_ALLOW_ORIGIN = "['localhost']"; #optional
+#       } // cfg.common_env;
+#     };
+#   };
 
   services.caddy.virtualHosts = {
     "hedgedoc.${config.networking.fqdn}".extraConfig = ''
